37fa9f8f7f2d2e934f42e04b03e88a4db0357c93
Zach Phillips <phillizr@udel.edu>
2023-11-18 02:44:14 -0500

    Added sklearn imports

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 
 msft = yf.Ticker("MSFT")
 
